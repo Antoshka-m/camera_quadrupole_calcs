03cd2a3c3b81a6594a51147e050e799dc7badec1
Antonio <anton.malovichko@epfl.ch>
2019-11-06 14:40:28 +0100

    Add calibration for both axis, improve input of parameters
    1.  Make calibration for both axis and each case: normalized (Il/Isum, Iz/Isum) and non-normalized (Il, Iz) signals. Do shift for each axis for the same value in px. Correct some typos in printing text messages.
    
    2. Show original first frame of video only once instead of twice.
    
    3. Read desired shift value in px and px-um coefficient from keyboard (instead of having parameters defined in the code), By default, have shift value=5 px.

--- calc_calib_line.py
+++ calc_calib_line.py
@@ -1,6 +1,6 @@
 from scipy.stats import linregress
 
-def calc_calib_line(x_shift, k_px_um, Il, Isum=None, normalization=False, shift_vs_sig=True):
+def calc_calib_line(x_shift, y_shift, k_px_um, Il, Iz, Isum=None, normalization=False, shift_vs_sig=True):
     """
     Calculate coefficient of calibration line shift to signal of signal to shift.
     Calculations done only on lateral axis (LR signal)
@@ -10,10 +10,14 @@ def calc_calib_line(x_shift, k_px_um, Il, Isum=None, normalization=False, shift_
     -------
     x_shift: np-array
         dx shifts of image
+    y_shift: np.array
+        dy shifts of image
     k_px_um: float
         px to um scale (from px size)
     Il: np.array
         LR (lateral) intensity difference signal
+    Iz: np.array
+        TB (vertical) intensity difference signal
     Isum: np.array
         sum intensity signal 
     normalization: boolean
@@ -27,18 +31,29 @@ def calc_calib_line(x_shift, k_px_um, Il, Isum=None, normalization=False, shift_
         intercept of linear fit
     """
     x_shift_nm = (x_shift/k_px_um)*1E3
+    y_shift_nm = (y_shift/k_px_um)*1E3
     if normalization:
         if shift_vs_sig:
-            k, b, _, _, _ = linregress(Il/Isum, x_shift_nm) #k, b for shift vs intensity, normalized over Isum
+            #calculate k, b for shift vs intensity, normalized over Isum for x
+            k_x, b_x, _, _, _ = linregress(Il/Isum, x_shift_nm) 
+            k_y, b_y, _, _, _ = linregress(Iz/Isum, y_shift_nm) 
         else:
-            k, b, _, _, _ = linregress(x_shift_nm, Il/Isum) #k, b for intensity vs shift, normalized over Isum
+            #k, b for intensity vs shift, normalized over Isum
+            k_x, b_x, _, _, _ = linregress(x_shift_nm, Il/Isum) 
+            k_y, b_y, _, _, _ = linregress(y_shift_nm, Iz/Isum) 
     else: # k, b for non-normalized intensity signal
         if shift_vs_sig:
-            k, b, _, _, _ = linregress(Il, x_shift_nm)
+            k_x, b_x, _, _, _ = linregress(Il, x_shift_nm)
+            k_y, b_y, _, _, _ = linregress(Iz, y_shift_nm)
         else:
-            k, b, _, _, _ = linregress(x_shift_nm, Il)
-    if b>0:
-        print('The calibration line (a.u. to um) has formula {}*x+{}'.format(round(k, 3), round(b, 3)))
+            k_x, b_x, _, _, _ = linregress(x_shift_nm, Il)
+            k_y, b_y, _, _, _ = linregress(y_shift_nm, Iz)
+    if b_x>=0:
+        print('The calibration line for x axis (a.u. to nm) has formula {}*x+{}\n'.format(round(k_x, 3), round(b_x, 3)))
     else:
-        print('The calibration line (a.u. to um) has formula {}*x{}'.format(round(k, 3), round(b, 3)))
-    return k, b
+        print('The calibration line for x axis (a.u. to nm) has formula {}*x{}\n'.format(round(k_x, 3), round(b_x, 3)))
+    if b_y>=0:
+        print('The calibration line for y axis (a.u. to nm) has formula {}*x+{}\n'.format(round(k_y, 3), round(b_y, 3)))
+    else:
+        print('The calibration line for y axis (a.u. to nm) has formula {}*x{}\n'.format(round(k_y, 3), round(b_y, 3)))
+    return k_x, b_x, k_y, b_y
--- calib_image_shift_universal_py.py
+++ calib_image_shift_universal_py.py
@@ -22,9 +22,9 @@ file = file[0] # get just single file instead of list
 print('Importing file ', file)
 frame = skvideo.io.vread(file, num_frames=1) # import just first frame
 frame = rgb2gray(frame[0]) # get element instead of list, make grayscale
-plt.figure()
-plt.imshow(frame, cmap=plt.cm.gray)
-plt.show()
+# plt.figure()
+# plt.imshow(frame, cmap=plt.cm.gray)
+# plt.show()
 
 # Compensate angle if its needed
 
@@ -36,27 +36,40 @@ if angle != 0:
     frame=rotate(frame, angle)
 
 # Detect center of lightspot, show quadrants:
-
 centroid=threshold_centroid(frame)
+print('Showing first frame of video with quadrants...')
 plot_im_w_quadrants(frame, centroid, fig_title='1st frame with quadrants')
 
+
 # Demonstrate how shifted image looks like
-
-transform = AffineTransform(translation=(1, 0))
+print('Image shifted for 5px in each axis will look like this:')
+transform = AffineTransform(translation=(5, 5))
 shifted = warp(frame, transform, mode='constant', preserve_range=True)
-plot_im_w_quadrants(shifted, centroid, fig_title='1 px shift')
-
+plot_im_w_quadrants(shifted, centroid, fig_title='5 px shift')
+print('If you want to have max test shift as shown above, just press enter')
+print('(If lightspot is partially out of field of view, better to choose smaller shift)')
+print('Otherwise manually enter desired shift of image in px, and press enter')
+print('Note: the same shift will be used for each axis')
+max_shift = input()
+if max_shift is '':
+    max_shift = 5
+else:
+    max_shift = float(max_shift)
+k_px_um = input('Enter px to um coefficient (scale):\n')
+k_px_um = float(k_px_um)
 # Shift images along x axis
 
 shifted_im = []
 # specify parameters for calculations
 # generate dx value for linear shift
-x_shift = np.array([0.1*dx for dx in range(0, 11)])
-k_px_um = 1.36 # scale px to um
+# x_shift = np.array([0.1*dx for dx in range(0, max_shift+1)])
+dx=0.1
+x_shift = np.arange(0, max_shift*(1+dx), dx*max_shift)
+# k_px_um = 1.36 # scale px to um
 normalization = False # don't scale signal over SUM
 shift_vs_sig = True # calculate shift from signal, not other way
 for dx in x_shift:
-    transform = AffineTransform(translation=(dx, 0))  # shift along lateral axis
+    transform = AffineTransform(translation=(dx, dx))  # shift along both axis
     shifted_im.append(warp(frame,
                            transform,
                            mode='constant',
@@ -75,20 +88,37 @@ for i in range(len(shifted_im)):
                                     Isum)
 
 # Show calculated intensity difference vs displacement and get linear fit coefficients of the calibration:
-
+# without normalization
 plot_shift_curves(k_px_um=k_px_um,
                   Il=Il,
                   Iz=Iz,
                   Isum=Isum,
                   x_shift=x_shift,
-                  normalization=normalization,
+                  normalization=False,
                   shift_vs_sig=shift_vs_sig)
-k, b = calc_calib_line(x_shift=x_shift,
-                       k_px_um=k_px_um,
-                       Il=Il,
-                       normalization=normalization,
-                       shift_vs_sig=shift_vs_sig)
-
+k_x, b_x, k_y, b_y = calc_calib_line(x_shift=x_shift,
+                                     y_shift=x_shift,
+                                     k_px_um=k_px_um,
+                                     Il=Il,
+                                     Iz=Iz,
+                                     normalization=False,
+                                     shift_vs_sig=shift_vs_sig)
+# with normalization
+plot_shift_curves(k_px_um=k_px_um,
+                  Il=Il,
+                  Iz=Iz,
+                  Isum=Isum,
+                  x_shift=x_shift,
+                  normalization=True,
+                  shift_vs_sig=shift_vs_sig)
+k_x, b_x, k_y, b_y = calc_calib_line(x_shift=x_shift,
+                                     y_shift=x_shift,
+                                     k_px_um=k_px_um,
+                                     Il=Il,
+                                     Iz=Iz,
+                                     Isum=Isum,
+                                     normalization=True,
+                                     shift_vs_sig=shift_vs_sig)
 
 
 
